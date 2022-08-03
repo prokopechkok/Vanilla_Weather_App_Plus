6624a1f5e9ba1ea1dfb0a91725e5512d6b81a718
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2022-08-03 20:55:47 +0300

    add temp, city, descr, humid, wind elements to displayTemperature

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
-function displayTemperature(response){
-    let temperatureElement=document.querySelector(#temperature);
-}
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  console.log(response.data);
+}
+let city = "Lviv";
 let apiKey = "8438301216c2822a596249b61bb568d7";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
- axios.get(apiUrl).then(displayTemperature);
+axios.get(apiUrl).then(displayTemperature);
