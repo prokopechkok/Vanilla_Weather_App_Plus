0d9f7ad3e2cd8e61b63e588e019fdd1d71808b54
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2022-08-04 10:55:01 +0300

    added search engine to js

--- src/app.js
+++ src/app.js
@@ -54,7 +54,19 @@ function displayTemperature(response) {
 
   console.log(response.data);
 }
-let city = "Lviv";
-let apiKey = "8438301216c2822a596249b61bb568d7";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "8438301216c2822a596249b61bb568d7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Lviv");
